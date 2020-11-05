426a7a54c181798af44f6bff3eb48ba27e4be33f
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-04 19:22:29 -0600

    urls.py created in travel_wishlist directory

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Place(models.Model):
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
