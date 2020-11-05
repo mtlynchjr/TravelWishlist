d54d20a578aee70f54edb5e61a1cd3dc3e040f3f
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-05 09:25:25 -0600

    models commented

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Place(models.Model):
-    name = models.CharField(max_length=200)
-    visited = models.BooleanField(default=False)
+class Place(models.Model): # Create model
+    name = models.CharField(max_length=200) # Set character limit for place name
+    visited = models.BooleanField(default=False) # Default all places to not yet visited
 
     def __str__(self):
-        return f'{self.name}, visited? {self.visited}'
+        return f'{self.name}, visited? {self.visited}' # Display place name and ask if visited
