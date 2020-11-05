ded88e477907f5721c685d5e37235b024b57db06
“jy8474np” <jy8474np@go.minnstate.edu>
2020-11-04 20:04:57 -0600

    NewPlaceForm redirect and save added to place_list function.

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Place
+from .forms import NewPlaceForm
 
 def place_list(request):
+    if request.method == 'POST':
+        form = NewPlaceForm(request.POST)
+        place = form.save()
+        if form.is_valid:
+            place.save()
+            return redirect('place_list')
+
     places = Place.objects.filter(visited=False).order_by('name')
-    return render(request, 'travel_wishlist/wishlist.html', {'places' : places})
+    new_place_form = NewPlaceForm()
+    return render(request, 'travel_wishlist/wishlist.html', {'places' : places , 'new_place_form' : new_place_form})
